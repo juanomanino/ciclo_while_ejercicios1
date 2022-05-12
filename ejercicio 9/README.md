3a58b5f3e7d12bafdefe81de492a8c1389ac8642
juanomanino <juannino@colegioguanenta.edu.co>
2022-05-12 17:59:43 -0500

    update

---
+++ ejercicio 9/README.md
@@ -0,0 +1,3 @@
+# Ejercicio 9: Sumatoria de los primeros N números naturales:
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ ejercicio 9/ejercicio9_while.py
@@ -0,0 +1,15 @@
+# Bucle While
+
+suma=0
+j=1
+print("Dame el valor de n: ")
+n=int(input())
+
+while j<=n:
+    suma=suma+j
+    j=j+1
+
+#output
+print("La suma es: ",suma)
+
+#Fin
